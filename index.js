57b01d53603af47c201f4a2c86acdce1fb9b023e
Nicholas <nicholasjoven@gmail.com>
2022-06-12 14:19:41 -0400

    commit tree 7e8cd248740834da608031a1d9ef85d29e11dd48

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+const main = document.getElementById(`main`);
+
+main.remove();
+
+//
+
+const newHeader = document.createElement(`h1`);
+newHeader.id = `victory`;
+newHeader.className = `victory`;
+newHeader.textContent = `YOUR-NAME is the champion`;
+
+document.body.append(newHeader);
